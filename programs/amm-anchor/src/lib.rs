fec1f1f7bd3c10e683b0e6777637bdbf4a91d058
Abhi16016 <abhinavkale919913@gmail.com>
2025-08-16 03:14:05 +0530

    main

--- programs/amm-anchor/src/lib.rs
+++ programs/amm-anchor/src/lib.rs
@@ -2,15 +2,35 @@ use anchor_lang::prelude::*;
 
 declare_id!("4XqThpjUnqqeg4vESBgzexS15cjBp82jsFTcH7XzTxko");
 
+pub mod constant;
+pub mod errors;
+pub mod helper;
+pub mod instructions;
+pub mod state;
+
+use instruction::*;
+
 #[program]
 pub mod amm_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<InitializePool>, seed: u64) -> Result<()> {
+        ctx.accounts.init_pool(ctx.bumps, seed)?;
+        Ok(())
+    }
+
+    pub fn deposit_asset(ctx: Context<DepositAsset>, amount_a: u64, amount_b: u64) -> Result<()> {
+        ctx.accounts.deposit(amount_a, amount_b)?;
+        Ok(())
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_a: bool, amount: u64, min_slippage: u64) -> Result<()> {
+        ctx.accounts.swap(is_a, amount, min_slippage)?;
+        Ok(())
+    }
+
+    pub fn withdraw_asset(ctx: Context<Withdraw>, lp_amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(lp_amount)?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
