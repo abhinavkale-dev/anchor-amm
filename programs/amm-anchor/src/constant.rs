1f554fc9dddb5ab902fa764cb66efe4a4608059f
Abhi16016 <abhinavkale919913@gmail.com>
2025-08-18 02:56:13 +0530

    some changes

--- programs/amm-anchor/src/constant.rs
+++
@@ -1,2 +0,0 @@
-pub const POOL: &[u8] = b"liquid_pool";
-pub const MINT_LP: &[u8] = b"mint_lp";
--- programs/amm-anchor/src/lib.rs
+++ programs/amm-anchor/src/lib.rs
@@ -2,35 +2,31 @@ use anchor_lang::prelude::*;
 
 declare_id!("4XqThpjUnqqeg4vESBgzexS15cjBp82jsFTcH7XzTxko");
 
-pub mod constant;
-pub mod errors;
-pub mod helper;
-pub mod instructions;
-pub mod state;
-
-use instruction::*;
-
 #[program]
 pub mod amm_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<InitializePool>, seed: u64) -> Result<()> {
-        ctx.accounts.init_pool(ctx.bumps, seed)?;
+    pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
         Ok(())
     }
 
-    pub fn deposit_asset(ctx: Context<DepositAsset>, amount_a: u64, amount_b: u64) -> Result<()> {
-        ctx.accounts.deposit(amount_a, amount_b)?;
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
         Ok(())
     }
 
-    pub fn swap(ctx: Context<Swap>, is_a: bool, amount: u64, min_slippage: u64) -> Result<()> {
-        ctx.accounts.swap(is_a, amount, min_slippage)?;
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount: u64, min: u64) -> Result<()> {
         Ok(())
     }
 
-    pub fn withdraw_asset(ctx: Context<Withdraw>, lp_amount: u64) -> Result<()> {
-        ctx.accounts.withdraw(lp_amount)?;
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn lock(ctx: Context<Update>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn unlock(ctx: Context<Update>) -> Result<()> {
         Ok(())
     }
 }
