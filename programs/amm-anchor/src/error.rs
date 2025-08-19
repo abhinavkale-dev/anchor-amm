6fc4c92775d8c326cf3255f991474726a8d3fcbd
Abhi16016 <abhinavkale919913@gmail.com>
2025-08-20 02:20:39 +0530

    Errors

--- programs/amm-anchor/src/error.rs
+++ programs/amm-anchor/src/error.rs
@@ -1,19 +1,38 @@
 use anchor_lang::prelude::*;
+use constant_product_curve::CurveError;
 
 #[error_code]
 pub enum AMMError {
-    #[msg("Minimum amount not met")]
-    MinAmount,
-
-    #[msg("The asset ratio is not valid")]
-    NotValidAsset,
-
-    #[msg("The amount in vault or in mint supply is zero")]
-    AmountZero,
-
-    #[msg("Overflow Occured")]
+    #[msg("This pool is locked")]
+    PoolLocked,
+    #[msg("Invalid Amount")]
+    InvalidAmount,
+    #[msg("Slippage Exceeded")]
+    SlippageExceeded,
+    #[msg("Overflow Detected")]
     Overflow,
+    #[msg("Underflow Detected")]
+    Underflow,
+    #[msg("Invalid Authority")]
+    InvalidAuthority,
+    #[msg("Invalid Precision")]
+    InvalidPrecision,
+    #[msg("Insufficient Balance")]
+    InsufficientBalance,
+    #[msg("Zero Balance")]
+    ZeroBalance
+}
 
-    #[msg("Slippage Occured")]
-    SlippageLimitExceeded
+impl From<CurveError> for AMMError {
+    fn from(error: CurveError) -> AmmError {
+        match error {
+            CurveError::InvalidPrecision      => AmmError::InvalidPrecision,
+            CurveError::Overflow              => AmmError::Overflow,
+            CurveError::Underflow             => AmmError::Underflow,
+            CurveError::InvalidFeeAmount      => AmmError::InvalidFee,
+            CurveError::InsufficientBalance   => AmmError::Insufficientbalance,
+            CurveError::ZeroBalance           => AmmError::ZeroBalance,
+            CurveError::SlippageLimitExceeded => AmmError::SlippageExceded,
+        }
+    }
 }
