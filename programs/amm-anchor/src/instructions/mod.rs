5e43d2c245fad79dade7ceceac29d3922a2d20b7
Abhi16016 <abhinavkale919913@gmail.com>
2025-08-20 02:21:46 +0530

    mod

--- programs/amm-anchor/src/instructions/mod.rs
+++ programs/amm-anchor/src/instructions/mod.rs
@@ -2,8 +2,10 @@ pub mod deposit_asset;
 pub mod initialize_pool;
 pub mod swap;
 pub mod withdraw_asset;
+pub mod update;
 
-pub use deposit_asset::*;
-pub use initialize_pool::*;
-pub use swap::*;
-pub use withdraw_asset::*;
+pub use deposit_asset;
+pub use initialize_pool;
+pub use swap;
+pub use withdraw_asset;
+pub use update;
