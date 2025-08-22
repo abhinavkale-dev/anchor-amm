e881df3a02ec53a2d265161da8661cb5c5029bc4
Abhi16016 <abhinavkale919913@gmail.com>
2025-08-23 03:37:48 +0530

    lib

--- programs/amm-anchor/src/lib.rs
+++ programs/amm-anchor/src/lib.rs
@@ -1,3 +1,5 @@
+use anchor_lang::prelude::*;
+
 pub mod constants;
 pub mod error;
 pub mod instructions;
@@ -8,35 +10,44 @@ pub use error::*;
 pub use instructions::*;
 pub use state::*;
 
-use anchor_lang::prelude::*;
-
 declare_id!("4XqThpjUnqqeg4vESBgzexS15cjBp82jsFTcH7XzTxko");
 
 #[program]
 pub mod amm_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, seed: u64, fee: u16, authority: Option<Pubkey>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.initialize(seed, fee, authority, &ctx.bumps)?;
         Ok(())
     }
 
     pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)?;
         Ok(())
     }
 
     pub fn swap(ctx: Context<Swap>, is_x: bool, amount: u64, min: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount, min)?;
         Ok(())
     }
 
     pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, min_x, min_y)?;
         Ok(())
     }
 
     pub fn lock(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.lock()?;
         Ok(())
     }
 
     pub fn unlock(ctx: Context<Update>) -> Result<()> {
+        ctx.accounts.unlock()?;
         Ok(())
     }
 }
